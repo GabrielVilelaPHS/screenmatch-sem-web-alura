dff84846c24fec093cf044dd2cbd51446d81dcc8
Gabriel Vilela <gabrielvileladesousa@gmail.com>
2024-09-19 08:38:40 -0300

    3. Manipulando com fluxos as coleções de dados

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
-import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,13 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {//implementa i
 
 	@Override
 	public void run(String... args) throws Exception { //equivalente ao método main
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=e02a423e");
-		System.out.println(json);
-
-		ConverteDados conversor = new ConverteDados();
-
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
